59e0da1f01a91287a892b44c5ee71c96deb272db
peicc1617 <peicc1413@outlook.com>
2019-02-21 21:28:22 +0800

    第四次提交：表格创建完成，下一步图表显示。

---
+++ src/main/java/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static  void main(String[] args){
+        Integer a = 99, b = 99;
+        System.out.println(a == b);//1 false
+        Integer c = 100, d = 100;
+        System.out.println(c == d);//2 true
+    }
+}
--- src/main/webapp/js/tableEdit.js
+++ src/main/webapp/js/tableEdit.js
@@ -44,8 +44,282 @@ $(function() {
     });
 
 });
-//生成bootstrap-table表格
+//初始化生成bootstrap-table表格
 function generateTable() {
-    alert("生成表格");
+    console.log("生成表格");
+    $('#myBootstrapTable').bootstrapTable('destroy');
+    var inputs=$("#inputs").find("span.editable");
+    var tableColumns=new Array();
+    //添加选择框字段
+    tableColumns.push({
+        field:'state',
+        checkbox:"true",
+    });
+    for(var i=0;i<inputs.length;i++){
+        var titleName=inputs[i].innerHTML;
+        tableColumns.push({
+            field:'input'+i,
+            title:titleName,
+            align:"center",
+        });
+    }
+    //添加编辑
+    tableColumns.push(
+        { field:'action',
+            formatter:"actionFormatter",
+            events:"actionEvents",
+            title: "编辑",
+            align:"center",
+        });
 
+    $('#myBootstrapTable').bootstrapTable({
+        columns: tableColumns,
+    });
+}
+//添加行模态对话框加载
+function addRow() {
+    $("#inputItemAdd").empty();
+    var allcolumns = $('#myBootstrapTable').bootstrapTable('getVisibleColumns');
+    //第一列选择框和最后一列编辑框不需要显示在模态对话框中
+    for(var i=1;i<allcolumns.length-1;i++){
+        var html="<div class=\"col-lg-6\"> <div class=\"input-group\"> <span class=\"input-group-addon\">" +
+            allcolumns[i].title +
+            "</span> <input type=\"text\" class=\"form-control\" id=\"" +
+            allcolumns[i].field +
+            "\"</div></div>";
+        $("#inputItemAdd").append(html);
+    }
+    $("#itemAdd").modal('show');
+
+}
+//添加行数据
+function addItem() {
+    var allcolumns = $('#myBootstrapTable').bootstrapTable('getVisibleColumns');
+    rowdata=new Object();
+    for(var i=1;i<allcolumns.length-1;i++){
+
+        rowdata[allcolumns[i].field]=$("#"+allcolumns[i].field).val();
+
+    }
+    $('#myBootstrapTable').bootstrapTable('append', rowdata);
+
+}
+//删除行
+function deleteRow()
+{
+    var ids = $.map($('#myBootstrapTable').bootstrapTable('getSelections'), function (row) {
+        return row.state;
+    });
+    $('#myBootstrapTable').bootstrapTable('remove', {field: 'state', values: ids});
+}
+//编辑
+function actionFormatter(value, row, index) {
+    return [
+        '<a class="edit ml10" href="javascript:void(0)" title="Edit">',
+        '<i class="glyphicon glyphicon-edit"></i> 编辑',
+        '</a>'
+    ].join('');
+}
+var updateIndex = 1;
+window.actionEvents = {
+    'click .edit': function (e, value, row, index) {
+        //清空模态对话框
+        $("#dataEditGroup").empty();
+        var allcolumns = $('#myBootstrapTable').bootstrapTable('getVisibleColumns');
+        for (var i=1;i<allcolumns.length-1;i++) {
+            var title = allcolumns[i].title;
+            var id = allcolumns[i].field;
+            var value = row[id];
+            var html = "<div class=\"col-lg-6\"><div class=\"input-group\"><span class=\"input-group-addon\">" +
+                title +
+                "</span> <input type=\"text\" class=\"form-control\" value =\"" + value + "\"id=\"" + id +"\"></div></div>"
+            $("#dataEditGroup").append(html);
+        }
+        updateIndex = index;
+        $('#itemEdit').modal('show');
+    }
+};
+//编辑后添加到表格中
+function editItem() {
+    $('#itemEdit').modal('hide');
+    rowdata=new Object();
+    var updates = $("#dataEditGroup input");
+    for(var i=0;i<updates.length;i++){
+
+        rowdata[updates[i].id]=updates[i].value;
+
+    }
+    $('#myBootstrapTable').bootstrapTable('updateRow',{index: updateIndex, row: rowdata});
+}
+//获取表格相关信息
+function getTableInformation() {
+    //获取表格字段
+    var allColumnName=[];//数组存储表格字段信息
+    var allcolumns = $('#myBootstrapTable').bootstrapTable('getVisibleColumns');
+    for (var i=1;i<allcolumns.length-1;i++) {
+        var title = allcolumns[i].title;
+        var id = allcolumns[i].field;
+        allColumnName.push(title);
+    }
+    //获取表格数据
+    var tableData=$('#myBootstrapTable').bootstrapTable('getData');
+    var json=JSON.stringify(tableData);
+    var allData=[];
+    for(var i=0;i<tableData.length;i++){
+        for(var j=0;j<allColumnName.length;j++){
+            var member="input"+j;
+            allData.push(tableData[i][member]);
+        }
+    }
+    var num=$('#myBootstrapTable').bootstrapTable('getData').length;//获取表格记录数
+    var sourceData=[];
+
+    for(var m=0;m<num;m++){
+        rowdata=new Object();
+        for(var n=0;n<allColumnName.length;n++){
+            var member="input"+n;
+            if(n==0){
+                rowdata[allColumnName[n]]=tableData[m][member];
+            }
+            else{
+                rowdata[allColumnName[n]]=parseInt(tableData[m][member]);
+            }
+        }
+        sourceData.push(rowdata);
+    }
+
+}
+//画图
+function showGraph() {
+    //////////////////////
+    //获取表格字段
+    var allColumnName=[];//数组存储表格字段信息
+    var allcolumns = $('#myBootstrapTable').bootstrapTable('getVisibleColumns');
+    for (var i=1;i<allcolumns.length-1;i++) {
+        var title = allcolumns[i].title;
+        var id = allcolumns[i].field;
+        allColumnName.push(title);
+    }
+    //获取表格数据
+    var tableData=$('#myBootstrapTable').bootstrapTable('getData');
+    var json=JSON.stringify(tableData);
+    var allData=[];
+    for(var i=0;i<tableData.length;i++){
+        for(var j=0;j<allColumnName.length;j++){
+            var member="input"+j;
+            allData.push(tableData[i][member]);
+        }
+    }
+    var num=$('#myBootstrapTable').bootstrapTable('getData').length;//获取表格记录数
+    var sourceData=[];
+
+    for(var m=0;m<num;m++){
+        rowdata=new Object();
+        for(var n=0;n<allColumnName.length;n++){
+            var member="input"+n;
+            if(n==0){
+                rowdata[allColumnName[n]]=tableData[m][member];
+            }
+            else{
+                rowdata[allColumnName[n]]=parseInt(tableData[m][member]);
+            }
+
+        }
+        sourceData.push(rowdata);
+    }
+    //X轴刻度
+    var xData=[];
+    for(var i=0;i<sourceData.length;i++){
+        xData.push(sourceData[i]["指标1"]);
+    }
+    var seriesData=[];
+    for(var i=0;i<allColumnName.length-1;i++){
+        seriesData.push({type: 'bar'});
+    }
+    //////////////////////
+    //获取表格字段数据
+
+    var dom = document.getElementById("histogramCharts");
+    var myChart = echarts.init(dom);
+
+    var app = {};
+    option = null;
+    app.title = '折柱混合';
+
+    option = {
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+                type: 'cross',
+                crossStyle: {
+                    color: '#999'
+                }
+            }
+        },
+        toolbox: {
+            feature: {
+                dataView: {show: true, readOnly: false},
+                magicType: {show: true, type: ['line', 'bar']},
+                restore: {show: true},
+                saveAsImage: {show: true}
+            }
+        },
+        legend: {
+            // data:['蒸发量','降水量','平均温度']
+        },
+        xAxis: [
+            {
+                type: 'category',
+                // data: ['1月','2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月'],
+                data:xData,
+                axisPointer: {
+                    type: 'shadow'
+                }
+            }
+        ],
+        yAxis: [
+            {
+                type: 'value',
+                name: '水量',
+                min: 0,
+                max: 250,
+                interval: 50,
+                axisLabel: {
+                    formatter: '{value} ml'
+                }
+            },
+            {
+                type: 'value',
+                name: '温度',
+                min: 0,
+                max: 25,
+                interval: 5,
+                axisLabel: {
+                    formatter: '{value} °C'
+                }
+            }
+        ],
+        series: [
+            {
+                name:'蒸发量',
+                type:'bar',
+                data:[2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3]
+            },
+            {
+                name:'降水量',
+                type:'bar',
+                data:[2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3]
+            },
+            {
+                name:'平均温度',
+                type:'line',
+                yAxisIndex: 1,
+                data:[2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2]
+            }
+        ]
+    };
+    ;
+    if (option && typeof option === "object") {
+        myChart.setOption(option, true);
+    }
 }
