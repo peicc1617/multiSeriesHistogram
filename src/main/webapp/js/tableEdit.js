0eb8e4865630f983894a7845787c3250d86ef652
peicc1617 <peicc1413@outlook.com>
2019-02-22 12:23:59 +0800

    第五次提交：项目创建基本完成，下一步项目保存

--- src/main/webapp/js/tableEdit.js
+++ src/main/webapp/js/tableEdit.js
@@ -1,8 +1,8 @@
 //添加计算指标
 function addInputElement(){
-    var num=$("#inputs").children().length+1;
+    var num=$("#inputs").children().length;//表格第一列不算
     console.log("添加输入因素"+num);
-    var title="指标"+num;
+    var title="系列"+num;
     var html="<li class=\"item-blue clearfix ui-sortable-handle\" style=\"position: relative; opacity: 1; left: 0px; top: 0px; z-index: auto;\">"+
                 "<label class=\"inline\">"+
                 "<span class=\"editable editable-click editable-open\">"+ title + "</span>" +
@@ -47,7 +47,7 @@ $(function() {
 //初始化生成bootstrap-table表格
 function generateTable() {
     console.log("生成表格");
-    $('#myBootstrapTable').bootstrapTable('destroy');
+    // $('#myBootstrapTable').bootstrapTable('destroy');
     var inputs=$("#inputs").find("span.editable");
     var tableColumns=new Array();
     //添加选择框字段
@@ -151,44 +151,6 @@ function editItem() {
     }
     $('#myBootstrapTable').bootstrapTable('updateRow',{index: updateIndex, row: rowdata});
 }
-//获取表格相关信息
-function getTableInformation() {
-    //获取表格字段
-    var allColumnName=[];//数组存储表格字段信息
-    var allcolumns = $('#myBootstrapTable').bootstrapTable('getVisibleColumns');
-    for (var i=1;i<allcolumns.length-1;i++) {
-        var title = allcolumns[i].title;
-        var id = allcolumns[i].field;
-        allColumnName.push(title);
-    }
-    //获取表格数据
-    var tableData=$('#myBootstrapTable').bootstrapTable('getData');
-    var json=JSON.stringify(tableData);
-    var allData=[];
-    for(var i=0;i<tableData.length;i++){
-        for(var j=0;j<allColumnName.length;j++){
-            var member="input"+j;
-            allData.push(tableData[i][member]);
-        }
-    }
-    var num=$('#myBootstrapTable').bootstrapTable('getData').length;//获取表格记录数
-    var sourceData=[];
-
-    for(var m=0;m<num;m++){
-        rowdata=new Object();
-        for(var n=0;n<allColumnName.length;n++){
-            var member="input"+n;
-            if(n==0){
-                rowdata[allColumnName[n]]=tableData[m][member];
-            }
-            else{
-                rowdata[allColumnName[n]]=parseInt(tableData[m][member]);
-            }
-        }
-        sourceData.push(rowdata);
-    }
-
-}
 //画图
 function showGraph() {
     //////////////////////
@@ -202,43 +164,26 @@ function showGraph() {
     }
     //获取表格数据
     var tableData=$('#myBootstrapTable').bootstrapTable('getData');
-    var json=JSON.stringify(tableData);
-    var allData=[];
-    for(var i=0;i<tableData.length;i++){
-        for(var j=0;j<allColumnName.length;j++){
-            var member="input"+j;
-            allData.push(tableData[i][member]);
-        }
+    //图例（legend）
+    var legendArray=[];
+    for(var i=1;i<allColumnName.length;i++){
+        legendArray.push(allColumnName[i]);
     }
-    var num=$('#myBootstrapTable').bootstrapTable('getData').length;//获取表格记录数
-    var sourceData=[];
-
-    for(var m=0;m<num;m++){
-        rowdata=new Object();
-        for(var n=0;n<allColumnName.length;n++){
-            var member="input"+n;
-            if(n==0){
-                rowdata[allColumnName[n]]=tableData[m][member];
-            }
-            else{
-                rowdata[allColumnName[n]]=parseInt(tableData[m][member]);
-            }
-
-        }
-        sourceData.push(rowdata);
-    }
-    //X轴刻度
+    //X轴刻度(xAxis)
     var xData=[];
-    for(var i=0;i<sourceData.length;i++){
-        xData.push(sourceData[i]["指标1"]);
+    for(var i=0;i<tableData.length;i++){
+        xData.push(tableData[i]["input0"]);
     }
-    var seriesData=[];
-    for(var i=0;i<allColumnName.length-1;i++){
-        seriesData.push({type: 'bar'});
+    //系列数据(series)
+    seriesData=[];
+    for(var i=1;i<allColumnName.length;i++){
+        var rowData=[];
+        for(var j=0;j<tableData.length;j++){
+            rowData.push(parseFloat(tableData[j]["input"+i]));
+        }
+        seriesData.push({name:allColumnName[i],type:'bar',data:rowData});
     }
-    //////////////////////
-    //获取表格字段数据
-
+    //配置echarts图表
     var dom = document.getElementById("histogramCharts");
     var myChart = echarts.init(dom);
 
@@ -265,58 +210,19 @@ function showGraph() {
             }
         },
         legend: {
-            // data:['蒸发量','降水量','平均温度']
+                data:legendArray
         },
         xAxis: [
             {
                 type: 'category',
-                // data: ['1月','2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月'],
                 data:xData,
                 axisPointer: {
                     type: 'shadow'
                 }
             }
         ],
-        yAxis: [
-            {
-                type: 'value',
-                name: '水量',
-                min: 0,
-                max: 250,
-                interval: 50,
-                axisLabel: {
-                    formatter: '{value} ml'
-                }
-            },
-            {
-                type: 'value',
-                name: '温度',
-                min: 0,
-                max: 25,
-                interval: 5,
-                axisLabel: {
-                    formatter: '{value} °C'
-                }
-            }
-        ],
-        series: [
-            {
-                name:'蒸发量',
-                type:'bar',
-                data:[2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3]
-            },
-            {
-                name:'降水量',
-                type:'bar',
-                data:[2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3]
-            },
-            {
-                name:'平均温度',
-                type:'line',
-                yAxisIndex: 1,
-                data:[2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2]
-            }
-        ]
+        yAxis: {},
+        series:seriesData
     };
     ;
     if (option && typeof option === "object") {
